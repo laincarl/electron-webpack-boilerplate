95c6561a92685fcd43b22e226e85424ea3954218
laincarl <1047972853@qq.com>
2018-10-29 15:54:51 +0800

    [FIX]修复启动bug

--- electron.js
+++ electron.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -9,22 +10,23 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({ width: 900, height: 680 });
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './build/index.html')}`);
-    mainWindow.on('closed', () => mainWindow = null);
-    mainWindow.openDevTools();
+  console.log(isDev);
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './build/index.html')}`);
+  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.openDevTools();
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
