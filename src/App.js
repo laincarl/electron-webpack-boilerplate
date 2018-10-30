d7554afe33a29dae4e5395c0dff351e5f8a26050
laincarl <1047972853@qq.com>
2018-10-30 11:02:59 +0800

    [IMP]结构更改

--- src/App.js
+++ src/App.js
@@ -1,73 +1,20 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
-import usb from 'usb';
-import util from 'util';
-import { remote } from 'electron';
-import child_process from 'child_process';
-import loadURL from './load';
-
-const exec = util.promisify(child_process.exec);
-
-const { BrowserWindow } = remote;
-
-function show(content) {
-  document.getElementById('show').innerText += content;
-}
-async function execCommand(command) {
-  const obj = await exec(command);
-  show(obj);
-  console.log(obj, typeof (obj));
-  // console.log('stdout:', stdout);
-  // console.log('stderr:', stderr);
-}
-usb.on('attach', (device) => {
-  console.log(device);
-  show(device);
-  const screenShoot = 'adb devices';
-  execCommand(screenShoot);
-});
-
-usb.on('detach', (device) => {
-  console.log(device);
-});
-
-
+import {
+  HashRouter as Router, Route, Switch, 
+} from 'react-router-dom';
+import Main from './containers/Main';
+import Company from './containers/Company';
 // const { COPYFILE_EXCL } = fs.constants;
 class App extends Component {
-  newwin = () => {
-    // const modalPath = path.join(
-    //   "file://",
-    //   __dirname,
-    //   "index.html"
-    // );
-    // let win = new BrowserWindow({ frame: false });
-    let win = new BrowserWindow();
-
-    win.on('close', () => {
-      win = null;
-    });
-
-    loadURL(win, 'p');
-    win.show();
-    // window.open("http://www.baidu.com")
-  }
-
-  exec = () => {
-    const screenShoot = 'adb devices';
-    execCommand(screenShoot);
-  }
-
   render() {
     return (
-      <div className="App">
-        <button onClick={this.newwin}>新窗口</button>
-        <button onClick={this.exec}>查看安卓</button>
-        <Link to="/p" target="_blank">
-          c
-        </Link>
-        <p id="show" />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/p" component={Company} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import {
-  HashRouter as Router, Route, Switch, 
-} from 'react-router-dom';
 import App from './App';
-import Company from './Company';
+
 import './index.css';
 
-const PageSet = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/p" component={Company} />
-
-    </Switch>
-  </Router>
-);
-ReactDOM.render(<PageSet />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
